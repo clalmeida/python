bfc42c3c27f290020a1d799637006b6af81df94a
clalmeida <clalmeida2008@gmail.com>
2019-09-15 03:15:18 -0300

    erro linha 8

--- verificar_cpf.py
+++ verificar_cpf.py
@@ -1,2 +1,13 @@
 cpf = input('digite cpf: ')
 validos = ("0123456789")
+tamanho = len(cpf)
+if tamanho != 11:
+    print ('erro')
+else:
+    for posicao in range (0,12):
+        numero = cpf[posicao]
+        tacerto = validos.find(numero)
+    if tacerto != -1:
+        print ("erro em ", posicao)
+        temero = ("sim")
+print ("tem erro")
